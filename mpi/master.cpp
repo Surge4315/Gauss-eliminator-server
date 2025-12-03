b03647b3ed1e9b5cdac84bb0f15173f7501e3fd7
Surge4315 <surge.4315@gmail.com>
2025-12-03 23:52:38 +0100

    added basic version for mpi

---
+++ mpi/master.cpp
@@ -0,0 +1,107 @@
+#include <mpi.h>
+#include <iostream>
+#include <vector>
+#include <unistd.h>
+#include "macierz.h"
+using namespace std;
+
+void backSub(const vector<vector<double>> &mat)
+{
+    int N = mat.size();
+    vector<double> x(N);
+    for (int i = N - 1; i >= 0; i--)
+    {
+        x[i] = mat[i][N];
+        for (int j = i + 1; j < N; j++)
+            x[i] -= mat[i][j] * x[j];
+        x[i] = x[i] / mat[i][i];
+    }
+
+    cout << "\nRozwiązanie układu:\n";
+    for (int i = 0; i < N; i++)
+        cout << "x" << i + 1 << " = " << x[i] << endl;
+}
+
+int main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0)
+    {
+        int num_workers = 1;
+
+        // Macierz do przetworzenia
+        std::vector<std::vector<double>> testMatrix = {
+            {1, -2, 3, -7},
+            {3, 1, 4, 5},
+            {2, 5, 1, 18}};
+
+        MPI_Info info;
+        MPI_Info_create(&info);
+
+        MPI_Comm workers_comm;
+        MPI_Comm_spawn("./worker_gauss", MPI_ARGV_NULL, num_workers,
+                       info, 0, MPI_COMM_WORLD, &workers_comm, MPI_ERRCODES_IGNORE);
+
+        // Wysyłanie całej macierzy do workera
+        int rows = testMatrix.size();
+        int cols = testMatrix[0].size();
+
+        // Wysłanie wymiarów macierzy
+        MPI_Send(&rows, 1, MPI_INT, 0, 0, workers_comm);
+        MPI_Send(&cols, 1, MPI_INT, 0, 0, workers_comm);
+
+        // Wysłanie każdego wiersza
+        for (int i = 0; i < rows; i++)
+        {
+            MPI_Send(testMatrix[i].data(), cols, MPI_DOUBLE, 0, 0, workers_comm);
+        }
+
+        cout << endl;
+        // Odbieranie wyniku od workera
+        std::vector<std::vector<double>> result(rows);
+        for (int i = 0; i < rows; i++)
+        {
+            result[i].resize(cols);
+            MPI_Recv(result[i].data(), cols, MPI_DOUBLE, 0, 0, workers_comm, MPI_STATUS_IGNORE);
+        }
+        usleep(1000); // for synchronization
+        // Wyświetlanie wyniku
+        backSub(result);
+
+        usleep(1000); // for synchronization
+
+        MPI_Comm workers_comm2;
+        MPI_Comm_spawn("./worker_gauss_p", MPI_ARGV_NULL, num_workers,
+                       info, 0, MPI_COMM_WORLD, &workers_comm2, MPI_ERRCODES_IGNORE);
+
+        // Wysłanie wymiarów macierzy
+        MPI_Send(&rows, 1, MPI_INT, 0, 0, workers_comm2);
+        MPI_Send(&cols, 1, MPI_INT, 0, 0, workers_comm2);
+
+        // Wysłanie każdego wiersza
+        for (int i = 0; i < rows; i++)
+        {
+            MPI_Send(testMatrix[i].data(), cols, MPI_DOUBLE, 0, 0, workers_comm2);
+        }
+
+        // Odbieranie wyniku od drugiego workera (opcjonalnie)
+        std::vector<std::vector<double>> result2(rows);
+        for (int i = 0; i < rows; i++)
+        {
+            result2[i].resize(cols);
+            MPI_Recv(result2[i].data(), cols, MPI_DOUBLE, 0, 0, workers_comm2, MPI_STATUS_IGNORE);
+        }
+        usleep(1000); // for synchronization
+        // Wyświetlanie wyniku drugiego workera
+        backSub(result2);
+
+        MPI_Info_free(&info);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ mpi/worker_gauss.cpp
@@ -0,0 +1,44 @@
+#include <mpi.h>
+#include <iostream>
+#include <vector>
+#include "gauss.h"
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Pobierz communicator z parent procesem
+    MPI_Comm parent;
+    MPI_Comm_get_parent(&parent);
+    
+    if (parent == MPI_COMM_NULL) {
+        std::cerr << "Brak parent communicatora!" << std::endl;
+        MPI_Finalize();
+        return 1;
+    }
+
+    // Odbieranie wymiarów macierzy
+    int rows, cols;
+    MPI_Recv(&rows, 1, MPI_INT, 0, 0, parent, MPI_STATUS_IGNORE);
+    MPI_Recv(&cols, 1, MPI_INT, 0, 0, parent, MPI_STATUS_IGNORE);
+    
+    std::cout << "Worker otrzymał macierz " << rows << "x" << cols << std::endl;
+    
+    // Odbieranie całej macierzy
+    std::vector<std::vector<double>> matrix(rows, std::vector<double>(cols));
+    for(int i = 0; i < rows; i++) {
+        MPI_Recv(matrix[i].data(), cols, MPI_DOUBLE, 0, 0, parent, MPI_STATUS_IGNORE);
+    }
+
+    matrix = gaussianElimination(matrix);
+
+    // Wysyłanie wyniku z powrotem
+    for(int i = 0; i < rows; i++) {
+        MPI_Send(matrix[i].data(), cols, MPI_DOUBLE, 0, 0, parent);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
